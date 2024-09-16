6b4943c18130ddb80714e0feacdd42bf42e49845
saimajahan27 <ls9894894@gmail.com>
2024-09-16 17:17:53 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function myFunction(){
+    document.getElementById("name").innerHTML = "Gently";
+}
+
+
+function change(){
+    document.getElementById('demo').innerHTML = "Plan";
+}
+
+
+
+function myimage() {
+    document.getElementById('myimage').src='image/crazy.png'
+   }
+ 
+ function image(){
+     document.getElementById('myimage').src='image/mood-swing.png'
+ }
+
+
+
+
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+
+function friend(){
+    document.getElementById('friend-none').style.display="none"
+}
+
+
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
